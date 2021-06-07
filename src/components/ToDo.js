64964d0f32787f494190c074356422317475c2a9
HyunSJ <dudtod1596@gmail.com>
2021-06-08 03:13:03 +0900

    feat: Add new file
    Add src/components/ToDo.js
        : components of todos text, delete button, id
         link to Detail page by id
    Add src/store.js
        : export actionCreators with addToDo(text), deleteToDo(id)
    modified src/index,js
        : wrap the App comonents with Provider
    modified src/routes/Detail.js
        : show text and id of ToDo.js
    modified src/routes/Home.js
        : main page, show todo list

---
+++ src/components/ToDo.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { actionCreators } from "../store";
+
+function ToDo(text, onDelBtnClick, id) {
+  return (
+    <li>
+      <Link to={`/${id}`}>
+        {text} <button onClick={onDelBtnClick}>DEL</button>
+      </Link>
+    </li>
+  );
+}
+
+function mapDispatchToProps(dispatch, ownProps) {
+  return {
+    onDelBtnClick: () => dispatch(actionCreators.deleteToDo(ownProps.id)),
+  };
+}
+
+export default connect(null, mapDispatchToProps)(ToDo);
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
+import { actionCreators } from "../store";
+import ToDo from "../components/ToDo";
 
-function Home() {
+function Home({ toDos, addToDo }) {
   const [text, setText] = useState("");
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log(text);
+    addToDo(text);
+    setText("");
   };
   const onChange = (e) => {
     setText(e.target.value);
@@ -17,9 +21,22 @@ function Home() {
         <input type="text" value={text} onChange={onChange}></input>
         <button>Add</button>
       </form>
-      <ul></ul>
+      <ul>
+        {toDos.map((toDo) => (
+          <ToDo {...toDo} key={toDo.id} />
+        ))}
+      </ul>
     </>
   );
 }
 
-export default Home;
+function mapStateToProps(state) {
+  //store.getState()와 같은 효과
+  return { toDos: state }; // 가져온 state에 추가할 요소 설정 가능
+}
+function mapDispatchToProps(dispatch) {
+  return {
+    addToDo: (text) => dispatch(actionCreators.addToDo(text)),
+  };
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
